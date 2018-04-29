df2a6336a541b4cc204ae9108cf8b357ae6fa960
FlorenceLegrand <flo.legrand@sfr.fr>
2018-04-29 19:14:29 +0200

    commit initial

---
+++ js/anniversaire.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('.titre').on('click', function() {  
+    /*$list.append('<button href="#">tous nos voeux</button>');*/
+      $('.message1').show().on('click', function() {
+              $('.message2').show().on('click', function() {
+                      $('.message3').show().on('click', function() {
+                                $('body').css('background-image', 'url(image/imagedefond.png)');
+                                $('audio').attr("autoplay",'true');
+                                });;;;
+                      });;;
+              });;
+  });
+
+});
